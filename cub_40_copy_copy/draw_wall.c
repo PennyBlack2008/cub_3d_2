207f903e99d8f6938b8e00b97be8bd4a7c9db2bb
PennyBlack2008 <g2ang12@gmail.com>
2020-10-31 15:08:46 +0900

    need to fix

--- cub_40_copy_copy/draw_wall.c
+++ cub_40_copy_copy/draw_wall.c
@@ -24,29 +24,14 @@ double				get_render_spot(int i, t_ray *r, t_win *w)
 	return (x);
 }
 
-int					get_color_spr(int l, int j, double scale_h, t_win *w)
+int					get_color_spr(double x, double y, double scale_w, double scale_h, t_win *w, int k)
 {
 	int				color;
-	double			x;
 	double			px, py;
-	double			scale_w;
 
-
-	if (k == 1)
-		x = get_render_sprite_spot(i, r, w); // 100의 크기로 환산, 여기서 x 에 넣어줄 값을 정한다.
-	scale_w = w->wall.length / 64.0; //<-- 벽의 두 면이 하나가 되면 적용될 수 있음
-	// scale_w = w->wall.length / 64.0; // 여기가 문제이다. 미리 옆의 길이를 구하고 할 수 있으면 좋은데... 그게 안된다. 이건
-	px = floor(x / scale_w); // x 에서 받는 scale 은 100 -> 64이다.
-	py = floor(j / scale_h);
-	color = w->map.curr_tex[k][(int)(64.0 * py + px)];
-	// if (r[i].wall_NSEW == NORTH)
-	// 	return (0xF1F740);
-	// if (r[i].wall_NSEW == SOUTH)
-	// 	return (0x40F759);
-	// if (r[i].wall_NSEW == EAST)
-	// 	return (0x40F7D5);
-	// if (r[i].wall_NSEW == WEST)
-	// 	return (0xDB40F7);
+	px = floor(x / scale_w);
+	py = floor(y / scale_h);
+	color = w->map.curr_tex[k][(int)(w->tex[k].height * py + px)];
 	return (color);
 }
 
@@ -160,23 +145,24 @@ void			draw_a_sprite(int i, t_ray *r, t_win *w)
 	double		orgn_pjtd_height;
 	double		orgn_pjtd_width;
 	double		scale_h;
+	double		scale_w;
 	int			color;
 
 	dist_to_spr = hypot(r[i].spr_hit.x - w->player.x, r[i].spr_hit.y - w->player.y) * fabs(cos(r[i].ang - w->player.ang));
 	pjtd_height = w->wall.height * w->player.projected_plane / dist_to_spr;
 	// printf("pjtd_height: %f\n", pjtd_height);
 	orgn_pjtd_height = pjtd_height;
-	// orgn_pjtd_width = pjtd_height * w->aspect_ratio; // 가로 세로 비율을 통해 
-	pjtd_width = 400 * w->player.projected_plane / dist_to_spr;
+	// orgn_pjtd_width = pjtd_height * w->aspect_ratio; // 가로 세로 비율을 통해
+	pjtd_width = 300 * w->player.projected_plane / dist_to_spr;
 	orgn_pjtd_width = pjtd_width;
 	// printf("orgn_pjtd_width: %f\n", orgn_pjtd_width);
 	scale_h = orgn_pjtd_height / 64.0; // <--- segfault 날 수도.. 스케일은 해상도를 넘어가는 벽 높이를 해상도에 맞게 조정하기 전에 랜더링을 해야하기 때문에 이 상태에서 스케일 값을 저장.
-	
+	scale_w = orgn_pjtd_width / 64.0;
 	if (pjtd_height > w->R_height)
 		pjtd_height = w->R_height;
 	if (pjtd_width > w->R_width)
 		pjtd_width = w->R_width;
-	
+
 	int j;		j = 0;		int k;		k = (pjtd_height / 2) - 1;
 
 	// 중간인 500 은 위쪽 while 에서 처리
@@ -187,14 +173,19 @@ void			draw_a_sprite(int i, t_ray *r, t_win *w)
 		j = 0;
 		while (j < pjtd_height / 2 && i + l > 0) // 아래 쪽
 		{
-			color = get_color_spr(l, w);
-			my_mlx_pixel_put(&w->img, i + l, w->player.height + j, 0x47E9EE);
+			color = get_color_spr(pjtd_width / 2 + l, j, scale_w, scale_h, w, 1);
+			// my_mlx_pixel_put(&w->img, i + l, w->player.height + j, 0x47E9EE);
+			if (color != 0)
+				my_mlx_pixel_put(&w->img, i + l, w->player.height + j, color);
 			j++;
 		}
 		k = (pjtd_height / 2) - 1;
 		while (k > 0 && i + l > 0) // 위 쪽
 		{
-			my_mlx_pixel_put(&w->img, i + l, w->player.height - k, 0x47E9EE);
+			color = get_color_spr(pjtd_width / 2 + l, j, scale_w, scale_h, w, 1);
+			// my_mlx_pixel_put(&w->img, i + l, w->player.height - k, 0x47E9EE);
+			if (color != 0)
+				my_mlx_pixel_put(&w->img, i + l, w->player.height - k, color);
 			k--;
 		}
 		if (i + l > w->R_width)

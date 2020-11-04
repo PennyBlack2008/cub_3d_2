aabdca75da037d44bc7764932c1ae4765f016000
PennyBlack2008 <g2ang12@gmail.com>
2020-11-05 01:48:32 +0900

    night backup

--- cub_53/screenshot.c
+++ cub_53/screenshot.c
@@ -39,32 +39,34 @@ void								generateBitmapImage (
 	unsigned char* image, int height, int width, char* imageFileName)
 {
 	int								fd;
-    int widthInBytes = width * BYTES_PER_PIXEL;
-
-    unsigned char padding[3] = {0, 0, 0};
-    int paddingSize = (4 - (widthInBytes) % 4) % 4;
-
-    int stride = (widthInBytes) + paddingSize;
+	int								widthInBytes;
+	unsigned char					padding[3] = {0, 0, 0};
+    int								paddingSize;
+	int								stride;
+	unsigned char*					fileHeader;
+	unsigned char*					infoHeader;
+	int								i;
 
     // FILE* imageFile = fopen(imageFileName, "wb");
-
+	widthInBytes = width * BYTES_PER_PIXEL;
+	paddingSize = (4 - (widthInBytes) % 4) % 4;
+	stride = (widthInBytes) + paddingSize;
 	if ((fd = open("screenshot.bmp", O_WRONLY | O_CREAT
 									| O_TRUNC | O_APPEND)) < 0)
 		return (0);
-
-    unsigned char* fileHeader = createBitmapFileHeader(height, stride);
-    fwrite(fileHeader, 1, FILE_HEADER_SIZE, imageFile);
-
-    unsigned char* infoHeader = createBitmapInfoHeader(height, width);
-    fwrite(infoHeader, 1, INFO_HEADER_SIZE, imageFile);
-
-    int i;
-    for (i = 0; i < height; i++) {
-        fwrite(image + (i*widthInBytes), BYTES_PER_PIXEL, width, imageFile);
-        fwrite(padding, 1, paddingSize, imageFile);
-    }
-
-    fclose(imageFile);
+	fileHeader = createBitmapFileHeader(height, stride);
+	write(fd, fileHeader, ft_strlen(fileHeader));
+	infoHeader = createBitmapInfoHeader(height, width);
+	write(fd, infoHeader, ft_strlen(fileHeader));
+	i = 0;
+	while (i < height)
+	{
+		// fwrite(image + (i*widthInBytes), BYTES_PER_PIXEL, width, imageFile);
+		write(fd, image + (i*widthInBytes), ft_strlen(image + (i*widthInBytes)));
+		fwrite(padding, 1, paddingSize, imageFile);
+		i++;
+	}
+	fclose(imageFile);
 }
 
 unsigned char* createBitmapFileHeader (int height, int stride)
@@ -96,19 +98,19 @@ unsigned char* createBitmapFileHeader (int height, int stride)
 
 unsigned char* createBitmapInfoHeader (int height, int width)
 {
-    static unsigned char infoHeader[] = {
-        0,0,0,0, /// header size, biSize : 현재 구조체의 크기 (unsigned int, 4bytes)
-        0,0,0,0, /// image width, biWidth : 비트맵 이미지의 가로 크기 (int, 4bytes)
-        0,0,0,0, /// image height, biHeight : 비트맵 이미지의 세로 크기 (int, 4bytes)
-        0,0,     /// number of color planes, biPlanes : 사용하는 색상판의 수 (unsigned short, 2bytes)
-        0,0,     /// bits per pixel, biBitCount : 픽셀 하나를 표현하는 비트 수 (unsigned short, 2bytes)
-        0,0,0,0, /// compression, biCompression : 압축 방식 (unsigned int, 4bytes)
-        0,0,0,0, /// image size, biSizeImage : 비트맵 이미지의 픽셀 데이터 크기 (unsigned int, 4bytes)
-        0,0,0,0, /// horizontal resolution, biXpelsPerMeter : 그림의 가로 해상도(미터당 픽셀) (int, 4bytes)
-        0,0,0,0, /// vertical resolution, biYpelsPerMeter : 그림의 세로 해상도(미터당 픽셀) (int, 4bytes)
-        0,0,0,0, /// colors in color table, biClrUsed : 색상 테이블에서 실제 사용되는 색상 수 (unsigned int, 4bytes)
-        0,0,0,0 /// important color count, biClrImportant : 비트맵을 표현하기 위해 필요한 색상 인덱스 수 (unsigned int, 4bytes)
-    };
+	static unsigned char infoHeader[] = {
+		0,0,0,0, /// header size, biSize : 현재 구조체의 크기 (unsigned int, 4bytes)
+		0,0,0,0, /// image width, biWidth : 비트맵 이미지의 가로 크기 (int, 4bytes)
+		0,0,0,0, /// image height, biHeight : 비트맵 이미지의 세로 크기 (int, 4bytes)
+		0,0,     /// number of color planes, biPlanes : 사용하는 색상판의 수 (unsigned short, 2bytes)
+		0,0,     /// bits per pixel, biBitCount : 픽셀 하나를 표현하는 비트 수 (unsigned short, 2bytes)
+		0,0,0,0, /// compression, biCompression : 압축 방식 (unsigned int, 4bytes)
+		0,0,0,0, /// image size, biSizeImage : 비트맵 이미지의 픽셀 데이터 크기 (unsigned int, 4bytes)
+		0,0,0,0, /// horizontal resolution, biXpelsPerMeter : 그림의 가로 해상도(미터당 픽셀) (int, 4bytes)
+		0,0,0,0, /// vertical resolution, biYpelsPerMeter : 그림의 세로 해상도(미터당 픽셀) (int, 4bytes)
+		0,0,0,0, /// colors in color table, biClrUsed : 색상 테이블에서 실제 사용되는 색상 수 (unsigned int, 4bytes)
+		0,0,0,0 /// important color count, biClrImportant : 비트맵을 표현하기 위해 필요한 색상 인덱스 수 (unsigned int, 4bytes)
+	};
 
     infoHeader[ 0] = (unsigned char)(INFO_HEADER_SIZE); // biSize
     infoHeader[ 4] = (unsigned char)(width      ); // biWidth
@@ -122,5 +124,5 @@ unsigned char* createBitmapInfoHeader (int height, int width)
     infoHeader[12] = (unsigned char)(1);
     infoHeader[14] = (unsigned char)(BYTES_PER_PIXEL*8);
 
-    return infoHeader;
+    return (infoHeader);
 }

3f5da2881d6ba8b5ad5625967311a3203786767b
Jinwoo Kang <jikang@c1r5s4.42seoul.kr>
2020-11-11 17:19:01 +0900

    normed draw_sprite.c

--- cub_56/draw_sprite.c
+++ cub_56/draw_sprite.c
@@ -6,13 +6,14 @@
 /*   By: jikang <jikang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 17:09:17 by jikang            #+#    #+#             */
-/*   Updated: 2020/11/11 17:09:20 by jikang           ###   ########.fr       */
+/*   Updated: 2020/11/11 17:18:44 by jikang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int					get_color_spr(double x, double y, t_sprite_var *var, t_win *w)
+int					get_color_spr(double x, double y, t_sprite_var *var,
+		t_win *w)
 {
 	int				color;
 	double			px;
@@ -32,7 +33,8 @@ static void			draw_part_down(int i, int l, t_sprite_var *var, t_win *w)
 	j = 0;
 	while (j < var->pjtd_height / 2 && i + l > 0)
 	{
-		color = get_color_spr(var->o_pjtd_width / 2 + l, var->o_pjtd_height / 2 + j, var, w);
+		color = get_color_spr(var->o_pjtd_width / 2 + l, var->o_pjtd_height / 2
+				+ j, var, w);
 		if (color != 0 && i + l < w->R_width)
 			my_mlx_pixel_put(&w->img, i + l, w->player.height + j, color);
 		j++;
@@ -47,29 +49,30 @@ static void			draw_part_up(int i, int l, t_sprite_var *var, t_win *w)
 	k = var->pjtd_height / 2 - 1;
 	while (k > 0)
 	{
-		color = get_color_spr(var->o_pjtd_width / 2 + l, var->o_pjtd_height / 2 - k, var, w);
+		color = get_color_spr(var->o_pjtd_width / 2 + l, var->o_pjtd_height / 2
+			- k, var, w);
 		if (color != 0)
 			my_mlx_pixel_put(&w->img, i + l, w->player.height - k, color);
 		k--;
 	}
 }
 
-void				draw_sprite_pixels(int i, t_sprite_var *var, t_ray *r, t_win *w)
+void				draw_sprite_pixels(int i, t_sprite_var *var, t_ray *r,
+		t_win *w)
 {
 	int				l;
- 
+
 	l = -1 * var->pjtd_width / 2;
 	while (l < var->pjtd_width / 2)
-	{	
+	{
 		if (i + l > w->R_width)
 			break ;
-		if (i + l >= 0 && (hypot(r[i + l].hit.x - w->player.plot.x ,
-		 r[i + l].hit.y - w->player.plot.y) > hypot(r[i].spr_hit.x - 
-		 w->player.plot.x, r[i].spr_hit.y 
-			- w->player.plot.y)))
+		if (i + l >= 0 && (hypot(r[i + l].hit.x - w->player.plot.x,
+		r[i + l].hit.y - w->player.plot.y) > hypot(r[i].spr_hit.x -
+		w->player.plot.x, r[i].spr_hit.y - w->player.plot.y)))
 		{
 			draw_part_down(i, l, var, w);
-			draw_part_up(i, l, var, w);	
+			draw_part_up(i, l, var, w);
 		}
 		l++;
 	}
@@ -80,11 +83,12 @@ void				draw_a_sprite(int i, t_ray *r, t_win *w)
 	t_sprite_var	var;
 	int				color;
 
-	var.dist_to_spr = hypot(r[i].spr_hit.x - w->player.plot.x, r[i].spr_hit.y 
+	var.dist_to_spr = hypot(r[i].spr_hit.x - w->player.plot.x, r[i].spr_hit.y
 			- w->player.plot.y) * fabs(cos(r[i].ang - w->player.ang));
 	if (var.dist_to_spr < 30)
 		var.dist_to_spr = 30;
-	var.pjtd_height = w->wall.height * w->player.projected_plane / var.dist_to_spr;
+	var.pjtd_height = w->wall.height * w->player.projected_plane /
+		var.dist_to_spr;
 	var.o_pjtd_height = var.pjtd_height;
 	var.pjtd_width = 300 * w->player.projected_plane / var.dist_to_spr;
 	var.o_pjtd_width = var.pjtd_width;
